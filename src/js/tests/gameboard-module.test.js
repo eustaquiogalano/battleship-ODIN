af1500ec39f448130864ee7f74b1df981aa665e9
eustaquio <eustaquio.galano@gmail.com>
2025-09-26 13:40:33 +0800

    refactor: gather all test inside describe
    - use beforeAll()

--- src/js/tests/gameboard-module.test.js
+++ src/js/tests/gameboard-module.test.js
@@ -1,38 +1,62 @@
 import { createGameboard } from '../gameboard-module';
 import { createBattleship } from '../ship-module';
 
-test('Battleship placement in the board', () => {
-  const gameboard = createGameboard();
+describe('Gameboard tests', () => {
+  let gameboard;
+  let batShip1;
+  let batShip2;
+  let batShip3;
 
-  const batShip3 = createBattleship(3);
-  gameboard.placeBattleShip(
-    [
-      [1, 1],
-      [2, 1],
-      [3, 1],
-    ],
-    batShip3
-  );
+  beforeAll(() => {
+    gameboard = createGameboard();
+    batShip1 = createBattleship(1);
+    batShip2 = createBattleship(2);
+    batShip3 = createBattleship(3);
+  });
 
-  const batShip2 = createBattleship(2);
-  gameboard.placeBattleShip(
-    [
-      [3, 3],
-      [4, 3],
-    ],
-    batShip2
-  );
+  test('Battleship placement in the board', () => {
+    gameboard.placeBattleShip(
+      [
+        [1, 1],
+        [2, 1],
+        [3, 1],
+      ],
+      batShip3
+    );
 
-  const batShip1 = createBattleship(1);
-  gameboard.placeBattleShip([[0, 4]], batShip1);
+    gameboard.placeBattleShip(
+      [
+        [3, 3],
+        [4, 3],
+      ],
+      batShip2
+    );
 
-  const batShipSpecial = createBattleship(1);
-  gameboard.placeBattleShip([[0, 0]], batShipSpecial);
+    gameboard.placeBattleShip([[0, 4]], batShip1);
 
-  console.log(batShip3);
-  console.log(batShip2);
-  console.log(batShip1);
-  console.log(gameboard);
+    const batShipSpecial = createBattleship(1);
+    gameboard.placeBattleShip([[0, 0]], batShipSpecial);
 
-  // expect(gameboard.).toBe('a');
+    console.log(batShip3);
+    console.log(batShip2);
+    console.log(batShip1);
+    console.log(gameboard);
+
+    // expect(gameboard.).toBe('a');
+  });
+
+  test('Check hits on ships', () => {
+    const gameboard = createGameboard();
+
+    let isHit = gameboard.checkHitOnShips(
+      [
+        [0, 1],
+        [1, 1],
+        [2, 3],
+      ],
+      [1, 1]
+    );
+
+    expect(isHit).toBe(true);
+  });
 });

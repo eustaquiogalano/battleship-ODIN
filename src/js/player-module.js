a2d45004b6fbc43286c4e855b706fea0e089dc6d
eustaquio <eustaquio.galano@gmail.com>
2025-09-30 14:48:53 +0800

    refactor: add internal state setup
    - add gameboard and battleships internal state inside player class.

--- src/js/player-module.js
+++ src/js/player-module.js
@@ -1,18 +1,25 @@
+import { createGameboard } from './gameboard-module';
+import { createBattleship } from './ship-module';
+
 class Player {
-  constructor(name, gameboard) {
+  constructor(name, type, boardSize = 5) {
     this.name = name;
-    this.gameboard = gameboard;
+    this.type = type;
+    this.gameboard = createGameboard(boardSize);
+    this.battleships = [
+      createBattleship(1),
+      createBattleship(2),
+      createBattleship(3),
+    ];
   }
 }
 
-export function createHumanPlayer(name, gameboard) {
-  let humanPlayer = new Player(name, gameboard);
-  humanPlayer.type = 'HUMAN';
+export function createHumanPlayer(name) {
+  let humanPlayer = new Player(name, 'HUMAN');
   return humanPlayer;
 }
 
-export function createComputerPlayer(name, gameboard) {
-  let computerPlayer = new Player(name, gameboard);
-  computerPlayer.type = 'COMPUTER';
+export function createComputerPlayer(name) {
+  let computerPlayer = new Player(name, 'COMPUTER');
   return computerPlayer;
 }

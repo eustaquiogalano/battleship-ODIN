5984b069c9a1e1a2da24f535b402e7995298ef54
eustaquio <eustaquio.galano@gmail.com>
2025-10-05 11:10:39 +0800

    feat: add helper function and use import modules
    - create computer player
    - create placeDefaultShipCoordinates() function for placing the default
      coordinates of the ships in gameboard property of player object
    - use displayShip() function but only for human player

--- src/js/gameplay-module.js
+++ src/js/gameplay-module.js
@@ -1,8 +1,49 @@
-import { displayGrid } from './DOM-manipulator';
-import { createHumanPlayer } from './player-module';
+import { displayGrid, displayShips } from './DOM-manipulator';
+import { createComputerPlayer, createHumanPlayer } from './player-module';
 
 export function startGame(playerName, size) {
   let human = createHumanPlayer(playerName, size);
+  let computer = createComputerPlayer(size);
+
   displayGrid();
+  placeDefaultShipCoordinates(human, computer);
+  displayShips(human, '#user-board');
+
   return human;
+}
+
+function placeDefaultShipCoordinates(human, computer) {
+  human.gameboard.placeBattleShip([[0, 0]], human.battleships[0]);
+  human.gameboard.placeBattleShip(
+    [
+      [2, 1],
+      [2, 2],
+    ],
+    human.battleships[1]
+  );
+  human.gameboard.placeBattleShip(
+    [
+      [1, 4],
+      [2, 4],
+      [3, 4],
+    ],
+    human.battleships[2]
+  );
+
+  computer.gameboard.placeBattleShip([[4, 4]], computer.battleships[0]);
+  computer.gameboard.placeBattleShip(
+    [
+      [4, 1],
+      [4, 2],
+    ],
+    computer.battleships[1]
+  );
+  computer.gameboard.placeBattleShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+    ],
+    computer.battleships[2]
+  );
 }

e0633dc235b7ca4fad325a117e53549903d49581
eustaquio <eustaquio.galano@gmail.com>
2025-10-06 12:19:30 +0800

    test: test gameplay module methods

--- src/js/tests/gameplay-module.test.js
+++ src/js/tests/gameplay-module.test.js
@@ -1,7 +1,30 @@
 import { startGame } from '../gameplay-module';
+import { displayShips } from '../DOM-manipulator';
 
 describe('Test gameplay module methods', () => {
   test('players', () => {
-    console.log(startGame());
+    let john = startGame('John', 10);
+
+    john.gameboard.placeBattleShip([[0, 0]], john.battleships[0]);
+    john.gameboard.placeBattleShip(
+      [
+        [2, 1],
+        [2, 2],
+      ],
+      john.battleships[1]
+    );
+    john.gameboard.placeBattleShip(
+      [
+        [1, 4],
+        [2, 4],
+        [3, 4],
+      ],
+      john.battleships[2]
+    );
+
+    console.log(john.battleships[0].gameboardPlacement);
+    console.log(john.battleships[1].gameboardPlacement);
+    console.log(john.battleships[2].gameboardPlacement);
+    console.log(john.gameboard);
   });
 });

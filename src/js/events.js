4faa4d41bdced71c2c0befc57506c9d2277ee4bc
eustaquio <eustaquio.galano@gmail.com>
2025-10-08 01:05:15 +0800

    feat: add event listener on enemy-board
    - verify clicked element to only target the cells
    - import and use markTarget() to shade the target coordinate
    - import and use attackGameboard() to computer object to receive the
      user attackGameboard
    - import and use checkWinner() to check every turn if there is a winner

--- src/js/events.js
+++ src/js/events.js
@@ -1,7 +1,27 @@
-import { startGame } from './gameplay-module';
+import { markTarget } from './DOM-manipulator';
+import {
+  attackGameboard,
+  checkWinner,
+  getPlayers,
+  startGame,
+} from './game-controller';
+
+let human;
+let computer;
 
 export function initClickEvents() {
   document.querySelector('#start-button').addEventListener('click', () => {
     startGame();
+    ({ human, computer } = getPlayers());
+  });
+
+  document.querySelector('#enemy-board').addEventListener('click', event => {
+    if (!computer || !event.target.dataset.row) return;
+    markTarget(event);
+    attackGameboard(
+      [Number(event.target.dataset.row), Number(event.target.dataset.col)],
+      computer
+    );
+    checkWinner(human, computer);
   });
 }
